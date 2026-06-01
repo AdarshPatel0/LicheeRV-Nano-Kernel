f8e1c5d56339b45812e404d1f0a44f4ae55a0ae8
Adarsh Patel <adarsh.patel0@outlook.com>
2026-06-01 00:21:56 +0000

    commit

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,30 @@ core::arch::global_asm!(
     "#
 );
 
+unsafe extern "C" {
+    static _kernel_end: u8;
+    static _stack_top: u8;
+}
+
+#[global_allocator]
+pub static HEAP: buddy_system_allocator::LockedHeap<32> =
+    buddy_system_allocator::LockedHeap::<32>::empty();
+
 #[unsafe(no_mangle)]
 extern "C" fn kmain(_hartid: usize, _device_tree_binary: usize) -> ! {
-    sbi::system_reset::system_reset(
-        sbi::system_reset::ResetType::Shutdown,
-        sbi::system_reset::ResetReason::NoReason,
-    )
-    .unwrap();
+    let kernel_end_address = core::ptr::addr_of!(_kernel_end) as usize;
+    unsafe {
+        let mut heap = HEAP.lock();
+        heap.add_to_heap(kernel_end_address, kernel_end_address + 33554432);
+        drop(heap);
+    };
+
+    use sg200x_bsp::sdmmc::{PowerLevel, Sdmmc};
+    use sg200x_bsp::soc::{SD_DRIVER_BASE, TOP_BASE};
+    let sdmmc = unsafe { Sdmmc::new(SD_DRIVER_BASE, TOP_BASE) };
+    sdmmc.init().unwrap();
+    let buf = "Hello World!".as_bytes();
+    sdmmc.write_block(0, &buf).unwrap();
     timer_interrupt::set_time_quanta(1_000_000);
     unsafe {
         use riscv::{
