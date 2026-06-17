8dda7c4c6b9165d5885f4fff24e5f5314fab2883
Adarsh Patel <adarsh.patel0@outlook.com>
2026-06-17 23:56:44 +0000

    commit

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,8 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y rustup qemu-system-riscv64 u-boot-qemu dosfstools mtools u-boot-tools parted
+sudo apt install -y rustup qemu-system-riscv64 u-boot-qemu dosfstools mtools u-boot-tools fdisk
 
-rustup default stable 
+rustup default stable
 rustup target add riscv64gc-unknown-none-elf
 rustup component add llvm-tools
 cargo install cargo-binutils
