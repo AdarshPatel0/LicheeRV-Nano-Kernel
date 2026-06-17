29e34defacb727be6376c9b4e77e95460ee9c44a
Adarsh Patel <adarsh.patel0@outlook.com>
2026-06-17 05:54:01 +0000

    commit

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,6 +4,10 @@ sudo apt install -y rustup qemu-system-riscv64 u-boot-qemu dosfstools mtools u-b
 
 rustup default stable 
 rustup target add riscv64gc-unknown-none-elf
+rustup component add llvm-tools
+cargo install cargo-binutils
+
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
 
 qemu-img create -f raw disk.img 1G
 mkfs.vfat -F 32 disk.img
--- create-disk.sh
+++ create-disk.sh
@@ -5,6 +5,12 @@ set -e
 
 cargo build 
 
-qemu-img create -f raw disk.img 1G && mkfs.vfat -F 32 disk.img
+qemu-img create -f raw disk.img 1G
 
-mcopy -i disk.img target/riscv64gc-unknown-none-elf/debug/kernel ::
+mkfs.vfat -F 32 disk.img
+
+rust-objcopy --strip-all -O binary \
+  target/riscv64gc-unknown-none-elf/debug/kernel \
+  target/riscv64gc-unknown-none-elf/debug/kernel.bin
+
+mcopy -i disk.img target/riscv64gc-unknown-none-elf/debug/kernel.bin ::kernel.bin
