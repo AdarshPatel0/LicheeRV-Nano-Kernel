fbd039481cd61967bc005b47e4c66e2a5c5d01c7
Adarsh Patel <adarsh.patel0@outlook.com>
2026-06-18 05:11:06 +0000

    commit

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y rustup qemu-system-riscv64 u-boot-qemu dosfstools mtools u-boot-tools fdisk
+sudo apt install -y rustup qemu-system-riscv64 u-boot-qemu u-boot-tools dosfstools mtools parted
 
 rustup default stable
 rustup target add riscv64gc-unknown-none-elf
