b679f46ed87f096e95b55283261f08f87068906c
Adarsh Patel <adarsh.patel0@outlook.com>
2026-06-20 07:50:11 +0000

    commit

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,6 @@ unsafe extern "C" {
     static _stack_top: u8;
 }
 
-#[global_allocator]
-pub static HEAP: buddy_system_allocator::LockedHeap<32> =
-    buddy_system_allocator::LockedHeap::<32>::empty();
-
 core::arch::global_asm!(
     r#"
     .section .text.entry
@@ -30,6 +26,8 @@ core::arch::global_asm!(
 
 #[unsafe(no_mangle)]
 extern "C" fn kmain(_argc: usize, argv: *const *const core::ffi::c_char) -> ! {
+    println!("Initializing kernel...");
+
     let dtb_address_str = unsafe { core::ffi::CStr::from_ptr(*argv.add(1)) }
         .to_str()
         .unwrap();
@@ -37,29 +35,6 @@ extern "C" fn kmain(_argc: usize, argv: *const *const core::ffi::c_char) -> ! {
     let fdt_address = usize::from_str_radix(dtb_address_str, 16).unwrap();
 
     println!("Flattened device address: {:#x}", fdt_address);
-
-    let fdt = unsafe {
-        let device_tree_binary_header = core::slice::from_raw_parts(fdt_address as *const u32, 40);
-        let total_size = device_tree_binary_header.get(1).unwrap();
-        let device_tree_binary_data =
-            core::slice::from_raw_parts(fdt_address as *const u8, *total_size as usize);
-        fdt::Fdt::new(device_tree_binary_data).expect("Failed to parse full FDT")
-    };
-
-    let kernel_end_address = core::ptr::addr_of!(_kernel_end) as usize;
-    let memory_region = fdt.memory().regions().next().unwrap();
-    let base_address = memory_region.starting_address as usize;
-    let size = memory_region.size.unwrap();
-
-    unsafe {
-        HEAP.force_unlock();
-        HEAP.lock()
-            .add_to_heap(kernel_end_address, base_address + size);
-    };
-
-    println!("Heap initialized:");
-    println!("\tStart: {:#x}", kernel_end_address);
-    println!("\tEnd: {:#x}", base_address + size);
 
     timer_interrupt::set_time_quanta(1_000_000);
     unsafe {
