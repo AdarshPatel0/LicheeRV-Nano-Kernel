5a80dff1fe49674824ff1ab9507d4167aa8cfad6
Adarsh Patel <adarsh.patel0@outlook.com>
2026-06-20 20:44:04 +0000

    commit

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y rustup qemu-system-riscv u-boot-qemu u-boot-tools dosfstools mtools parted
+sudo apt install -y rustup
 
 rustup default stable
 rustup target add riscv64gc-unknown-none-elf
--- boot.cmd
+++
@@ -1,2 +0,0 @@
-load ${devtype} ${devnum}:${distro_bootpart} 0x80200000 kernel.bin
-go 0x80200000 ${fdtcontroladdr}
