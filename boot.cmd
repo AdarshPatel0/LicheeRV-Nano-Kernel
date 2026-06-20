ad3566dd0229a6f359971f3300afd4537472995f
Adarsh Patel <adarsh.patel0@outlook.com>
2026-06-20 08:43:41 +0000

    commit

--- boot.cmd
+++ boot.cmd
@@ -1,2 +1,2 @@
 load ${devtype} ${devnum}:${distro_bootpart} 0x80200000 kernel.bin
-go 0x80200000 ${fdt_addr}
+go 0x80200000 ${fdtcontroladdr}
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ unsafe extern "C" {
     static _stack_top: u8;
 }
 
+#[global_allocator]
+pub static HEAP: buddy_system_allocator::LockedHeap<32> = buddy_system_allocator::LockedHeap::<32>::empty();
+
 core::arch::global_asm!(
     r#"
     .section .text.entry
@@ -26,15 +29,32 @@ core::arch::global_asm!(
 
 #[unsafe(no_mangle)]
 extern "C" fn kmain(_argc: usize, argv: *const *const core::ffi::c_char) -> ! {
-    println!("Initializing kernel...");
-
-    let dtb_address_str = unsafe { core::ffi::CStr::from_ptr(*argv.add(1)) }
-        .to_str()
-        .unwrap();
+    let dtb_address_str = unsafe { core::ffi::CStr::from_ptr(*argv.add(1)) }.to_str().unwrap();
 
     let fdt_address = usize::from_str_radix(dtb_address_str, 16).unwrap();
 
     println!("Flattened device address: {:#x}", fdt_address);
+
+    let fdt = unsafe {
+        let device_tree_binary_header = core::slice::from_raw_parts(fdt_address as *const u32, 40);
+        let total_size = device_tree_binary_header.get(1).unwrap();
+        let device_tree_binary_data = core::slice::from_raw_parts(fdt_address as *const u8, *total_size as usize);
+        fdt::Fdt::new(device_tree_binary_data).expect("Failed to parse full FDT")
+    };
+
+    let kernel_end_address = core::ptr::addr_of!(_kernel_end) as usize;
+    let memory_region = fdt.memory().regions().next().unwrap();
+    let base_address = memory_region.starting_address as usize;
+    let size = memory_region.size.unwrap();
+
+    unsafe {
+        HEAP.force_unlock();
+        HEAP.lock().add_to_heap(kernel_end_address, base_address + size);
+    };
+
+    println!("Heap initialized:");
+    println!("\tStart: {:#x}", kernel_end_address);
+    println!("\tEnd: {:#x}", base_address + size);
 
     timer_interrupt::set_time_quanta(1_000_000);
     unsafe {
@@ -42,14 +62,8 @@ extern "C" fn kmain(_argc: usize, argv: *const *const core::ffi::c_char) -> ! {
             interrupt,
             register::{self, stvec},
         };
-        register::stvec::write(riscv::register::stvec::Stvec::new(
-            trap_handler::entry::trap_handler_entry as *const u8 as usize,
-            stvec::TrapMode::Direct,
-        ));
-        println!(
-            "Supervisor trap: Direct@{:#x}",
-            trap_handler::entry::trap_handler_entry as *const u8 as usize
-        );
+        register::stvec::write(riscv::register::stvec::Stvec::new(trap_handler::entry::trap_handler_entry as *const u8 as usize, stvec::TrapMode::Direct));
+        println!("Supervisor trap: Direct@{:#x}", trap_handler::entry::trap_handler_entry as *const u8 as usize);
         interrupt::enable();
         println!("Interrupts enabled.");
         interrupt::enable_interrupt(interrupt::Interrupt::SupervisorTimer);
@@ -66,9 +80,6 @@ extern "C" fn kmain(_argc: usize, argv: *const *const core::ffi::c_char) -> ! {
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     print::print!("{}", info);
-    let _ = sbi::system_reset::system_reset(
-        sbi::system_reset::ResetType::Shutdown,
-        sbi::system_reset::ResetReason::SystemFailure,
-    );
+    let _ = sbi::system_reset::system_reset(sbi::system_reset::ResetType::Shutdown, sbi::system_reset::ResetReason::SystemFailure);
     loop {}
 }
